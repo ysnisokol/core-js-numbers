aefb8ecacff96933c054882a745e0ddd4d3d188c
Andrew Zhylinski <ysnisokol@gmail.com>
2024-11-23 01:12:05 +0300

    feat: add solution

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -197,8 +197,11 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(num) {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
 }
 
 /**
